0a3361a2c96e5e30dd2706eb251e48a50ab62f23
therluuuu <lutherzega@student.ub.ac.id>
2022-05-17 21:28:46 +0700

    Initial commit

---
+++ login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <link rel="stylesheet" href="metodePembayaran.css" />
+    <title>Document</title>
+</head>
+<body>
+    <div class="pilihan_login">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card text-black" style="border-radius: 1rem; background-color: #F9FAFB;">
+                        <div class="card-body p-5">
+                            <div class="mb-md-5 mt-md-4 pb-5">
+                                <h2 class="judul">Motobuddies</h2>
+                                <p class="ket_login">Masuk sebagai</p>
+                                <div class="tombol1">
+                                    <button class="button_montir">MONTIR</button>
+                                </div>
+                                <p class="pil_login">atau</p>
+                                <div class="tombol2">
+                                    <button class="button_pengguna">PENGGUNA</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ login2.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class = "landing_page">
+    <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div class="card bg-white text-black" style="border-radius: 1rem;">
+              <div class="card-body p-5">
+    
+                <div class="mb-md-5 mt-md-4 pb-5">
+    
+                  <h2 class="fw-bold mb-2 text-uppercase text-black">Masuk</h2>
+                  <p class="text-black-50 mb-5">Selamat datang di aplikasi MotoBuddies, kepercayaan dan kemudahan pengguna adalah nomor satu!</p>
+    
+                  <div class="form-outline form-white mb-4">
+                    <input type="email" id="typeEmailX" class="form-control form-control-lg" placeholder="Email"/>
+
+                  </div>
+    
+                  <div class="form-outline form-white mb-4">
+                    <input type="password" id="typePasswordX" class="form-control form-control-lg" placeholder="Password"/>
+                  </div>
+    
+                  <p class="small mb-5 pb-lg-2"><a class="text-blue-50 fw-bold" href="#!">Lupa password?</a></p>
+                </div>
+                
+                <div class = "card-body text-center">
+                    <button class="button_masuk" type="submit">MASUK</button>
+
+                    <p class="mb-0">Belum punya akun? <a href="#!" class="text-blue-50 fw-bold">Daftar</a>
+                    </p>
+                  </div>
+                </div>
+    
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</body>
+</html>
+
+</html>
